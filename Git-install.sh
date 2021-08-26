70266a06537fec2d3637ebc5812aad6f5dfef812
C0deMakr <84343107+C0deMakr@users.noreply.github.com>
2021-08-25 19:35:33 -0500

    Added Wget

--- Git-install.sh
+++ Git-install.sh
@@ -2,7 +2,7 @@ while true; do
     read -p "First we have to install Homebrew, is that okay? Y or N? If you choose N this script will exit." yn
     case $yn in
         [Yy]* ) /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) echo Exiting!; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
@@ -11,7 +11,15 @@ while true; do
     read -p "Alright, homebrew is installed! ready to install git? Y or N? If you choose N, this script will exit." yn
     case $yn in
         [Yy]* ) brew install git; break;;
-        [Nn]* ) exit;;
+        [Nn]* ) echo Exiting!; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+while true; do
+    read -p "And done! would you like to install wget too? like before, if you answer N then this script will exit" yn
+    case $yn in
+        [Yy]* ) brew install wget; break;;
+        [Nn]* ) echo Exiting!; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
